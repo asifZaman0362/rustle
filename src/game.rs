37eb9051d9db7708f6f6279a51a32a9f91d975bf
asifZaman0362 <zero362001@protonmail.com>
2022-09-22 22:24:20 +0530

    Added rocket. moved code to web kindof

--- src/game.rs
+++ src/game.rs
@@ -3,21 +3,21 @@ use rand::Rng;
 use std::fs::File;
 use std::io::{prelude::*, BufReader, BufWriter};
 
-struct UserData<'a> {
+struct UserData {
     guess_distribution: Vec<(u8, usize)>,
-    path: &'a str
+    path: String
 }
 
-impl<'a> UserData<'a> {
+impl UserData {
     fn new(path: &str) -> UserData {
         let guess_distribution: Vec<(u8, usize)> = vec![ (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0) ];
-        let path = path;
+        let path = path.to_owned();
         UserData { guess_distribution, path }
     }
 
     fn load(filepath: &str) -> std::io::Result<UserData> {
         let datafile = File::open(&filepath)?;
-        let path = filepath;
+        let path = filepath.to_owned();
         let mut guess_distribution = vec![];
         let reader = BufReader::new(datafile);
         for line in reader.lines() {
@@ -35,7 +35,7 @@ impl<'a> UserData<'a> {
     }
 
     fn dump(&self) -> std::io::Result<usize> {
-        let datafile = File::create(self.path)?;
+        let datafile = File::create(&self.path)?;
         let mut writer = BufWriter::new(datafile);
         let mut bytes: usize = 0;
         for (tries, freq) in &self.guess_distribution {
@@ -51,14 +51,15 @@ enum GameResult {
     Lose(String),
 }
 
-pub struct Game<'a> {
+pub struct Game {
     dictionary: Vec<String>,
     word_pool: Vec<String>,
-    data: UserData<'a>
+    data: UserData,
+    chosen: String
 }
 
-impl<'a> Game<'a> {
-    pub fn new(dictionary_path: &str, word_pool_path: &str, data_path: &'a str) -> std::io::Result<Game<'a>> {
+impl Game {
+    pub fn new(dictionary_path: &str, word_pool_path: &str, data_path: &str) -> std::io::Result<Game> {
         let data = match UserData::load(data_path) {
             Ok(data) => data,
             Err(_) => {
@@ -68,10 +69,9 @@ impl<'a> Game<'a> {
         };
         let dictionary = Game::load_dictionary(dictionary_path)?;
         let word_pool = Game::load_words(word_pool_path)?;
+        let chosen = "".to_owned();
         Ok(Game {
-            dictionary,
-            word_pool,
-            data
+            dictionary, word_pool, data, chosen
         })
     }
 
@@ -96,14 +96,15 @@ impl<'a> Game<'a> {
         Ok(words)
     }
 
-    fn get_random_word(&self) -> &String {
+    pub fn get_random_word(&mut self) -> &String {
         let mut rng = rand::thread_rng();
         let random_word_number = rng.gen_range(0..self.word_pool.len());
-        &self.word_pool[random_word_number as usize]
+        self.chosen = self.word_pool[random_word_number as usize].as_str().to_owned();
+        &self.chosen
     }
 
-    fn check(input: &str, answer: &str) -> [i8; 5] {
-        let upper = answer.to_uppercase();
+    pub fn check(&self, input: &str) -> [i8; 5] {
+        let upper = self.chosen.to_uppercase();
         let mut matches = [0; 5];
         let mut mask: Vec<usize> = vec![];
         for (i, a) in input.chars().enumerate() {
@@ -127,107 +128,15 @@ impl<'a> Game<'a> {
         string.trim().to_owned()
     }
 
-    fn validate_input(&self, guess: String) -> Result<String, &'static str> {
+    pub fn validate_input(&self, guess: &String) -> Result<String, &'static str> {
         if guess.len() > 5 {
             Err("Your guess must be exactly 5 letters long!")
         } else {
             match self.dictionary.binary_search(&guess) {
-                Ok(_) => Ok(guess),
+                Ok(_) => Ok(guess.clone()),
                 Err(_) => Err("I've never seen that word before!")
             }
         }
     }
 
-    fn start_game(&self) -> GameResult {
-        let answer = self.get_random_word();
-        for i in 0..6 {
-            let guess = loop {
-                println!("\nEnter your guess:");
-                match self.validate_input(self.get_input()) {
-                    Ok(guess) => {
-                        break guess
-                    }
-                    Err(message) => {
-                        println!("{}", message);
-                        continue;
-                    }
-                };
-            };
-            let matches = Game::check(&guess, answer);
-            let mut correct = 0;
-            let col_white = Fixed(255);
-            let col_gray_bg = Fixed(242);
-            let text_style_correct = Style::new().bold().on(Green).fg(col_white);
-            let text_style_misplaced = Style::new().bold().on(Yellow).fg(col_white);
-            let text_style_invalid = Style::new().bold().on(col_gray_bg).fg(col_white);
-            for (i, result) in matches.into_iter().enumerate() {
-                match result {
-                    1 => {
-                        correct += 1;
-                        print!(
-                            "{}",
-                            text_style_correct.paint(format!(
-                                "[{}]",
-                                guess
-                                    .chars()
-                                    .nth(i as usize)
-                                    .expect("index out of bounds!")
-                            ))
-                        );
-                    }
-                    0 => print!(
-                        "{}",
-                        text_style_misplaced.paint(format!(
-                            "[{}]",
-                            guess
-                                .chars()
-                                .nth(i as usize)
-                                .expect("index out of bounds!")
-                        ))
-                    ),
-                    -1 => print!(
-                        "{}",
-                        text_style_invalid.paint(format!(
-                            "[{}]",
-                            guess
-                                .chars()
-                                .nth(i as usize)
-                                .expect("index out of bounds!")
-                        ))
-                    ),
-                    _ => panic!("Shouldn't have happened!"),
-                };
-            }
-            if correct == 5 {
-                return GameResult::Win(answer.to_owned(), i);
-            }
-        }
-        GameResult::Lose(answer.to_owned())
-    }
-
-    pub fn play(&mut self) {
-        let remarks = vec![ "Genius", "Marvelous", "Amazing", "Nice", "Passable", "Whew" ];
-        loop {
-            match self.start_game() {
-                GameResult::Win(_, tries) => {
-                    println!("{}, you win!", remarks[tries as usize]);
-                    self.data.guess_distribution[tries as usize].1 += 1;
-                    match self.data.dump() {
-                        Ok(_) => {},
-                        Err(err) => panic!("Failed to save user data: {}", err)
-                    };
-                },
-                GameResult::Lose(word) => {
-                    println!("Oof! The word was {}. Better luck next time!", word);
-                }
-            }
-            println!("Would you like to play again? (y/Y)");
-            let mut input = String::new();
-            std::io::stdin().read_line(&mut input).expect("failed to read standard input!");
-            match input.trim() {
-                "y" | "Y" => continue,
-                _ => break,
-            };
-        }
-    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,46 @@
+#[macro_use] extern crate rocket;
+use game::Game;
+use rocket::State;
+
 mod game;
 
-fn main() {
-    println!("\n\n\nWelcome to rustle: terminal wordle written in rust\n\n");
-    println!("The rules are simple:\n\tGuess the random word in a maximum of six attempts to win the game!\n");
+#[get("/")]
+async fn index(game_state: &State<game::Game>) -> String {
+    "Hello, World".to_owned()
+}
+
+#[get("/guess/<word>")]
+async fn guess(game_state: &State<game::Game>, word: String) -> String {
+    match game_state.validate_input(&word) {
+        Ok(_) => {
+            let guess_result = game_state.check(&word);
+            let mut correct = 0;
+            for i in guess_result.into_iter() {
+                if i == 1 {
+                    correct += 1;
+                }
+            }
+            if correct == 5 {
+                return "Correct!".to_owned();
+            } else {
+                return "Incorrect!".to_owned();
+            }
+        },
+        Err(mssg) => mssg.to_owned()
+    }
+}
+
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
     let mut game = match game::Game::new("./dict.txt", "./repo.txt", "./user_data.txt") {
         Ok(game) => game,
         Err(mssg) => panic!("Failed to start game: {}", mssg)
     };
-    game.play();
+    println!("random word: {}", game.get_random_word());
+    let _rocket = rocket::build()
+        .manage(game)
+        .mount("/", routes![index, guess])
+        .launch()
+        .await?;
+    Ok(())
 }
